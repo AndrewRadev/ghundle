8c765dd4050be4a42e2a3272632c4f25df7546ea
Andrew Radev <andrey.radev@gmail.com>
2013-06-30 14:38:38 +0200

    Remove obsolete usage message

--- lib/githooks/app_error.rb
+++ lib/githooks/app_error.rb
@@ -1,16 +1,7 @@
 module Githooks
   class AppError < RuntimeError
     def usage
-      <<EOF
-Usage:
-
-  githooks <command> [options...]
-
-  <command> can currently be only "run", so
-
-  githooks run <hook-name>
-
-EOF
+      ''
     end
   end
 end
